a294aa21545b59a9fbf8a64864a290e3931c31af
RiChiara <Chiararicci2011@hotmail.it>
2025-10-21 13:54:39 +0200

    up

---
+++ Progetti/Esercizi in Python/L3-consegna.py
@@ -0,0 +1,20 @@
+#Genera un nome per la tua band
+
+città= input("dimmi la tua città natale : " " ")
+nome=input("come si chiama la tua iguana: " " ")
+a= 'Modena'
+
+print ("Ora, proviamo ad inventare un nome per la nostra mitica band!!!!!")
+if città == (a):
+ print("SIIIII!!! allora la band si chiamerà: """, città+nome)
+else: 
+   print("Visto che non hai scelto Modena, prendiamo in considerazione solo il nome dell'animale" " ", nome)
+
+
+
+    
+  
+  
+
+
+
